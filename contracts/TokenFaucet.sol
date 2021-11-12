fec13bd0d9d2fc7ffc12e0acba68bf71c992f8b4
Android Studio <devandroid@ort>
2021-11-12 09:13:26 -0300

    added ADMIN funcions

--- contracts/TokenFaucet.sol
+++ contracts/TokenFaucet.sol
@@ -2,28 +2,35 @@ pragma solidity ^0.8.0;
 
 import "./IERC20Extended.sol";
 
+// Multi-Token Faucet
 contract TokenFaucet {
-    address public owner;
+    address public owner; // owner of the faucet
+    uint256 public timer = 1 days; // timer of dispense time
+    uint256 public  dispenseValue = 1; // value for every dispense
 
-    uint256 public constant dispenseValue = 1;
+    mapping(address => mapping(address => uint256)) public cannotDispenseUntil; // time until user can dispense for every token
 
-    mapping(address => mapping(address => uint256)) public cannotDispenseUntil;
-
-    modifier onceADay(IERC20Extended token, address to) {
-        require(cannotDispenseUntil[address(token)][msg.sender] < block.timestamp, "You must wait a day between dispenses");
+    // verification account already asked for a dispense before enough time has passed
+    modifier timePassed(IERC20Extended token, address to) {
+        require(cannotDispenseUntil[address(token)][msg.sender] < block.timestamp, "Not enough time between dispenses");
         _;
-        cannotDispenseUntil[address(token)][msg.sender] = block.timestamp + 1 days;
-    }
+        cannotDispenseUntil[address(token)][msg.sender] = block.timestamp + timer;
+    }  
 
+    // modifier for owner-only functions
     modifier onlyOwner() {
         require(msg.sender == owner, "Owner-only function");
         _;
-    }
+    } 
 
-    function dispense(IERC20Extended token, address to) public onceADay(token, to) {
+    // ask for dispense
+    function dispense(IERC20Extended token, address to) public timePassed(token, to) {
         token.transfer(to, dispenseValue);
     }
 
+    // ADMIN FUNCTIONS:
+
+    // arbitrary withdrawal of any token (owner-only)
     function withdraw(
         IERC20Extended token,
         address to,
@@ -32,6 +39,17 @@ contract TokenFaucet {
         token.transfer(to, value);
     }
 
+    // select timer reset time (owner-only)
+    function selectTime(uint256 _timer) public onlyOwner {        
+        timer = _timer;
+    }
+
+    // select dispense value (owner-only)
+    function selectDispenseValue(uint256 _dispenseValue) public onlyOwner {        
+        dispenseValue = _dispenseValue;
+    }
+
+    // constructor
     constructor() public {
         owner = msg.sender;
     }
