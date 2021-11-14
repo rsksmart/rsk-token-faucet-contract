81e5a8909a513068b02a74e2a2c11921a091c4d0
FedeAbdo15 <federicoabdo04@gmail.com>
2021-11-14 18:56:17 -0300

    added owner tests + setOwner function

--- contracts/TokenFaucet.sol
+++ contracts/TokenFaucet.sol
@@ -6,7 +6,7 @@ import "./IERC20Extended.sol";
 contract TokenFaucet {
     address public owner; // owner of the faucet
     uint256 public timer = 1 days; // timer of dispense time
-    uint256 public  dispenseValue = 10000000000000000000; // value for every dispense
+    uint256 public dispenseValue = 10000000000000000000; // value for every dispense
     string public constant msgError = "Not enough time between dispenses"; // message error for timePassed() modifier
     string public constant msgOwner = "Owner-only function"; // message error for onlyOwner() modifier
 
@@ -14,23 +14,31 @@ contract TokenFaucet {
 
     // verification account already asked for a dispense before enough time has passed
     modifier timePassed(IERC20Extended token, address to) {
-        require(cannotDispenseUntil[address(token)][msg.sender] < block.timestamp, msgError);
+        require(
+            cannotDispenseUntil[address(token)][msg.sender] < block.timestamp,
+            msgError
+        );
         _;
-        cannotDispenseUntil[address(token)][msg.sender] = block.timestamp + timer;
-    }  
+        cannotDispenseUntil[address(token)][msg.sender] =
+            block.timestamp +
+            timer;
+    }
 
     // modifier for owner-only functions
     modifier onlyOwner() {
         require(msg.sender == owner, msgOwner);
         _;
-    } 
+    }
 
     // ask for dispense
-    function dispense(IERC20Extended token, address to) public timePassed(token, to) {
+    function dispense(IERC20Extended token, address to)
+        public
+        timePassed(token, to)
+    {
         token.transfer(to, dispenseValue);
     }
 
-    // ADMIN FUNCTIONS:
+    // OWNER FUNCTIONS:
 
     // arbitrary withdrawal of any token (owner-only)
     function withdraw(
@@ -41,14 +49,19 @@ contract TokenFaucet {
         token.transfer(to, value);
     }
 
-    // select timer reset time (owner-only)
-    function selectTime(uint256 _timer) public onlyOwner {        
+    // set timer reset time (owner-only)
+    function setTimer(uint256 _timer) public onlyOwner {
         timer = _timer;
     }
 
-    // select dispense value (owner-only)
-    function selectDispenseValue(uint256 _dispenseValue) public onlyOwner {        
+    // set dispense value (owner-only)
+    function setDispenseValue(uint256 _dispenseValue) public onlyOwner {
         dispenseValue = _dispenseValue;
+    }
+
+    // set owner (owner-only)
+    function setOwner(address _owner) public onlyOwner {
+        owner = _owner;
     }
 
     // constructor
--- test/TokenFaucet.test.js
+++ test/TokenFaucet.test.js
@@ -41,6 +41,59 @@ contract("Token Faucet", (accounts) => {
     assert(balance.eq(dispenseValue));
   });
 
+  it("dispense second time after timers done", async () => {
+    await this.faucet.dispense(this.tuki.address, accounts[1], {
+      from: accounts[1],
+    });
+    const timer = (await this.faucet.timer()) * 1 + 1;
+    const balanceShouldHave = web3.utils.toBN(
+      2 * (await this.faucet.dispenseValue())
+    );
+    // act
+    await timeMachine.advanceTimeAndBlock(timer);
+    await this.faucet.dispense(this.tuki.address, accounts[1], {
+      from: accounts[1],
+    });
+    const balance = await this.tuki.balanceOf(accounts[1]);
+    //console.log("balance", balance);
+    //console.log("balanceShouldHAve", balanceShouldHave);
+    // assert
+    assert(balance.eq(balanceShouldHave));
+  });
+
+  it("owner changes timers value", async () => {
+    const newTimer = web3.utils.toBN(10);
+    //act
+    await this.faucet.setTimer(newTimer, {
+      from: accounts[0],
+    });
+    const timer = await this.faucet.timer();
+    // assert
+    assert(timer.eq(newTimer));
+  });
+
+  it("owner changes dispenseValue", async () => {
+    const newDispenseValue = web3.utils.toBN(10);
+    //act
+    await this.faucet.setDispenseValue(newDispenseValue, {
+      from: accounts[0],
+    });
+    const dispenseValue = await this.faucet.dispenseValue();
+    // assert
+    assert(dispenseValue.eq(newDispenseValue));
+  });
+
+  it.only("owner changes owner", async () => {
+    const newOwner = accounts[1];
+    //act
+    await this.faucet.setOwner(newOwner, {
+      from: accounts[0],
+    });
+    const owner = await this.faucet.owner();
+    // assert
+    assert.equal(owner, newOwner);
+  });
+
   it("does not allow double dispense", async () => {
     await this.faucet.dispense(this.tuki.address, accounts[1], {
       from: accounts[1],
@@ -56,42 +109,42 @@ contract("Token Faucet", (accounts) => {
     );
   });
 
-  it("dispense second time after timers done", async () => {
-    await this.faucet.dispense(this.tuki.address, accounts[1], {
-      from: accounts[1],
-    });
-    const timer = (await this.faucet.timer()) * 1 + 1;
-    const balanceShouldHave = web3.utils.toBN(
-      2 * (await this.faucet.dispenseValue())
-    );
-    // act
-    await timeMachine.advanceTimeAndBlock(timer);
-    await this.faucet.dispense(this.tuki.address, accounts[1], {
-      from: accounts[1],
-    });
-    const balance = await this.tuki.balanceOf(accounts[1]);
-    console.log("balance", balance);
-    console.log("balanceShouldHAve", balanceShouldHave);
-    // assert
-    assert(balance.eq(balanceShouldHave));
-  });
-  it("owner changes timers value", async () => {
+  it("does not allow to change timers value by not owner", async () => {
     const newTimer = web3.utils.toBN(10);
-    await this.faucet.selectTime(newTimer, {
-      from: accounts[0],
-    });
-    const timer = await this.faucet.timer();
-    // assert
-    assert(timer.eq(newTimer));
+    const msgOwner = await this.faucet.msgOwner();
+    //atc, assert
+    await assert.rejects(
+      () =>
+        this.faucet.setTimer(newTimer, {
+          from: accounts[1],
+        }),
+      msgOwner
+    );
   });
 
-  it("owner changes dispenseValue", async () => {
+  it("does not allow to change dispenseValue by not owner", async () => {
     const newDispenseValue = web3.utils.toBN(10);
-    await this.faucet.selectDispenseValue(newDispenseValue, {
-      from: accounts[0],
-    });
-    const dispenseValue = await this.faucet.dispenseValue();
-    // assert
-    assert(dispenseValue.eq(newDispenseValue));
+    const msgOwner = await this.faucet.msgOwner();
+    //atc, assert
+    await assert.rejects(
+      () =>
+        this.faucet.setDispenseValue(newDispenseValue, {
+          from: accounts[1],
+        }),
+      msgOwner
+    );
+  });
+
+  it("does not allow to change owner by not owner", async () => {
+    const newOwner = accounts[1];
+    const msgOwner = await this.faucet.msgOwner();
+    //atc, assert
+    await assert.rejects(
+      () =>
+        this.faucet.setOwner(newOwner, {
+          from: accounts[1],
+        }),
+      msgOwner
+    );
   });
 });
