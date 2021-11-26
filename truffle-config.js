e53ddd09ae6eb5e2c19bc29c27bbbc56b8c46c10
FedeAbdo15 <83824693+FedeAbdo15@users.noreply.github.com>
2021-11-26 09:07:28 -0300

    added BalanceFetcher.sol and BalanceFetcher.test.js

--- truffle-config.js
+++ truffle-config.js
@@ -28,6 +28,8 @@ try {
   console.log("No .secret")
 }
 module.exports = {
+  plugins: ["solidity-coverage"],
+
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
